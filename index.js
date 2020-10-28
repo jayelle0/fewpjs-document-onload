91946c93b1e1d48b313ff766dbf95f80f326c11f
jayelle0 <you@example.com>
2020-10-27 22:06:42 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+     updateDOM() 
+  })
+
+ function updateDOM() {
+    document.querySelector("p#text").innerHTML = "This is really cool!"
+ }
+
+//  document.addEventListener( "DOMContentLoaded", function () {
+//     updateDOM()
+//   } );
+  
+  
+//   function updateDOM() {
+//     document.getElementById( "text" )
+//       .innerHTML = "This is really cool!";
+//   }
